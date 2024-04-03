55657ab1f6e686e324ad2d7ab15d3176a8af0330
Alberto Damo <alberto.damo@gmail.com>
2024-04-03 09:51:27 +0200

    added lib c_input_check to Makefile and sources

--- Makefile
+++ Makefile
@@ -3,6 +3,11 @@ CC = gcc
 
 c_queue_src_path = $(or $(C_QUEUE_PATH), .)
 
+ifndef $(C_CHECK_INPUT_ROOT)
+C_CHECK_INPUT_ROOT := $(c_queue_src_path)/lib/c_input_check
+include $(C_CHECK_INPUT_ROOT)/Makefile
+endif
+
 c_queue.c = $(c_queue_src_path)/c_queue.c
 
 all: c_queue.o
--- c_queue.c
+++ c_queue.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "lib/c_input_check/c_check_input.h"
+
 #define NOT_IMPLMENTED perror("not implemented")
 
 struct c_queue{
@@ -12,20 +14,20 @@ struct c_queue{
     uint8_t data[];
 };
 
-static uint8_t check_input_pointer(const void* ptr, const char* ptr_name){
-    if (!ptr) {
-        fprintf(stderr, "ERROR: NULL pointer %s\n", ptr_name);
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
-}
+// static uint8_t !c_check_input_pointer(const void* ptr, const char* ptr_name){
+//     if (!ptr) {
+//         fprintf(stderr, "ERROR: NULL pointer %s\n", ptr_name);
+//         return EXIT_FAILURE;
+//     }
+//     return EXIT_SUCCESS;
+// }
 
 //public
 uint8_t 
 c_queue_push(c_queue** queue, const void* ele, const uint16_t ele_size)
 {
-    if (check_input_pointer(queue, "c_queue root pointer")) return EXIT_FAILURE;
-    if (check_input_pointer(ele, "c_queue element to push")) return EXIT_FAILURE;
+    if (!c_check_input_pointer(queue, "c_queue root pointer")) return EXIT_FAILURE;
+    if (!c_check_input_pointer(ele, "c_queue element to push")) return EXIT_FAILURE;
     if (!ele_size){
         fprintf(stderr, "ERROR: invalid data size, must be > 0"); 
         return EXIT_FAILURE;
@@ -33,7 +35,7 @@ c_queue_push(c_queue** queue, const void* ele, const uint16_t ele_size)
 
     if (!(*queue)) {
         *queue = calloc(1, sizeof(**queue) + ele_size);
-        if (check_input_pointer(*queue, "c_queue new c_queue")) return EXIT_FAILURE;
+        if (!c_check_input_pointer(*queue, "c_queue new c_queue")) return EXIT_FAILURE;
         memcpy(&(*queue)->data[0], ele, ele_size);
         (*queue)->next=NULL;
         (*queue)->ele_size=ele_size;
@@ -45,13 +47,13 @@ c_queue_push(c_queue** queue, const void* ele, const uint16_t ele_size)
 uint8_t 
 c_queue_pop(c_queue** queue, void** out)
 {
-    if (check_input_pointer(queue, "c_queue root pointer")) return EXIT_FAILURE;
-    if (check_input_pointer(out, "out root pointer")) return EXIT_FAILURE;
-    if (check_input_pointer(*queue, "c_queue is empty")) return EXIT_FAILURE;
+    if (!c_check_input_pointer(queue, "c_queue root pointer")) return EXIT_FAILURE;
+    if (!c_check_input_pointer(out, "out root pointer")) return EXIT_FAILURE;
+    if (!c_check_input_pointer(*queue, "c_queue is empty")) return EXIT_FAILURE;
 
     if (!(*out)) {
         (*out) = calloc(1, (*queue)->ele_size);
-        if (check_input_pointer(*out, "c_queue new out buffer")) return EXIT_FAILURE;
+        if (!c_check_input_pointer(*out, "c_queue new out buffer")) return EXIT_FAILURE;
     }
 
     void* data = &(*queue)->data[0];
@@ -73,7 +75,7 @@ c_queue_is_empty(const c_queue* queue)
 
 void c_queue_free(c_queue* queue)
 {
-    if (check_input_pointer(queue, "c_queue root pointer")) return ;
+    if (!c_check_input_pointer(queue, "c_queue root pointer")) return ;
     c_queue* temp = queue->next;
 
     free(queue);
